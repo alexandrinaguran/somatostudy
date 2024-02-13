75d44f8a84e35440f73c9032d24a9b4e8f995ba0
Alexandrina Guran <102151299+alexandrinaguran@users.noreply.github.com>
2024-02-13 16:30:42 +0100

    secondlevel analysis scripts

---
+++ FIR_2ndlevel_LeftTouch_1v4.m
@@ -0,0 +1,38 @@
+%-----------------------------------------------------------------------
+% Job saved on 20.07.2023
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'P:\userdata\gurana93\data\somatosensation\data_glm\secondlevel\FIR\left1v4'};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Alaska\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Asta\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Grady\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Lilly\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Mia\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Velvet\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Sahibou\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Georgi\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Milo\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Zserbo\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Kiki\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Edgar\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Ace\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Joker\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Benny\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Timo\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\KKnopfi\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Maylo\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Kazuo\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Ori\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Kalea\con_0012.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Toffee\con_0012.nii,1'
+                                                          }; %% add further dogs later on: maylo, Knopfi. 
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
---
+++ FIR_2ndlevel_RightTouch_1v3.m
@@ -0,0 +1,38 @@
+%-----------------------------------------------------------------------
+% Job saved on 20.07.2023FIR
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'P:\userdata\gurana93\data\somatosensation\data_glm\secondlevel\FIR\right1v3'};
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = {
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Alaska\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Asta\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Grady\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Lilly\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Mia\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Velvet\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Sahibou\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Georgi\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Milo\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Zserbo\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Kiki\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Edgar\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Ace\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Joker\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Benny\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Timo\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\KKnopfi\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Maylo\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Kazuo\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Ori\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Kalea\con_0003.nii,1'
+                                                          'P:\userdata\gurana93\data\somatosensation\data_glm\firstlevel_scrub_dogHRF\smoothed\FIR\Toffee\con_0003.nii,1'
+                                                          }; %% add further dogs later on: maylo, Knopfi. 
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
